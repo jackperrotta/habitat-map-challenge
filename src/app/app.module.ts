8726655323059ac7c47cd91ce327a70d4309db76
jackperrotta <tug24657@temple.edu>
2021-03-14 15:15:35 -0400

    add angular material components and layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
+
+// Angular material
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+
+
 
 // Componenets
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { TaskEnterComponent } from './components/task-enter/task-enter.component';
-import { TaskListComponent } from './components/task-list/task-list.component';
-import { TaskMapComponent } from './components/task-map/task-map.component';
 
 // Third Party
 // import { Loader } from "@googlemaps/js-api-loader"
@@ -18,15 +24,19 @@ import { TaskMapComponent } from './components/task-map/task-map.component';
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent,
-    TaskEnterComponent,
-    TaskListComponent,
-    TaskMapComponent
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatGridListModule,
+    MatAutocompleteModule,
+    MatFormFieldModule,
+    MatInputModule,
+    DragDropModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit, } from '@angular/core';
-import { Loader } from "@googlemaps/js-api-loader"
+import {FormControl} from '@angular/forms';
+import {Observable} from 'rxjs';
+import {map, startWith} from 'rxjs/operators';
+import { Loader } from "@googlemaps/js-api-loader";
+import {CdkDragDrop, moveItemInArray} from '@angular/cdk/drag-drop';
+
 
 @Component({
   selector: 'dashboard',
@@ -8,6 +13,22 @@ import { Loader } from "@googlemaps/js-api-loader"
 })
 export class DashboardComponent implements OnInit {
   map;
+  myControl = new FormControl();
+  options: string[] = ['One', 'Two', 'Three'];
+  filteredOptions: Observable<string[]>;
+
+  movies = [
+    'Episode I - The Phantom Menace',
+    'Episode II - Attack of the Clones',
+    'Episode III - Revenge of the Sith',
+    'Episode IV - A New Hope',
+    'Episode V - The Empire Strikes Back',
+    'Episode VI - Return of the Jedi',
+    'Episode VII - The Force Awakens',
+    'Episode VIII - The Last Jedi',
+    'Episode IX – The Rise of Skywalker'
+  ];
+
   constructor() { }
 
   ngOnInit(){
@@ -18,11 +39,75 @@ export class DashboardComponent implements OnInit {
     });
     
     loader.load().then(() => {
+      const directionsService = new google.maps.DirectionsService();
+      const directionsRenderer = new google.maps.DirectionsRenderer();
+
       this.map = new google.maps.Map(document.getElementById("map") as HTMLElement, {
-        center: { lat: -34.397, lng: 150.644 },
-        zoom: 8,
+        center: { lat: 39.953350, lng: -75.145610 },
+        zoom: 12,
       });
+
+      directionsRenderer.setMap(this.map);
+
+      calculateAndDisplayRoute(
+        directionsService,
+        directionsRenderer
+      );
+
+      // const onChangeHandler = function () {
+      //   calculateAndDisplayRoute(directionsService, directionsRenderer);
+      // };
+      // (document.getElementById("start") as HTMLElement).addEventListener(
+      //   "change",
+      //   onChangeHandler
+      // );
+      // (document.getElementById("end") as HTMLElement).addEventListener(
+      //   "change",
+      //   onChangeHandler
+      // );
     });
+
+    function calculateAndDisplayRoute(
+      directionsService: google.maps.DirectionsService,
+      directionsRenderer: google.maps.DirectionsRenderer
+    ) {
+      directionsService.route(
+        {
+          origin: {
+            // query: (document.getElementById("start") as HTMLInputElement).value,
+            query: "philadelphia, pa"
+          },
+          destination: {
+            // query: (document.getElementById("end") as HTMLInputElement).value,
+            query: "los angeles, ca"
+          },
+          travelMode: google.maps.TravelMode.DRIVING,
+        },
+        (response, status) => {
+          if (status === "OK") {
+            directionsRenderer.setDirections(response);
+          } else {
+            window.alert("Directions request failed due to " + status);
+          }
+        }
+      );
+    }
+
+    this.filteredOptions = this.myControl.valueChanges
+      .pipe(
+        startWith(''),
+        map(value => this._filter(value))
+      );
+  }
+
+  private _filter(value: string): string[] {
+    const filterValue = value.toLowerCase();
+
+    return this.options.filter(option => option.toLowerCase().includes(filterValue));
+  }
+
+  drop(event: CdkDragDrop<string[]>) {
+    moveItemInArray(this.movies, event.previousIndex, event.currentIndex);
   }
 
 }
